5450e8a1570f8432b1c5f7182dcb1e467a28d172
srebalaji <srebalaji@outlook.com>
2017-07-14 21:20:58 +0530

    React Props
    1. Adding props in components.
    2. Adding default values to props.
    3. Adding props types.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ const App = () => <h1>This is stateless</h1>
 
 class State extends React.Component {
   render() {
+    let txt = this.props.txt
+    let cat = this.props.cat
     return (
       <div>
         <h1>Stateful</h1>
+        <h3>{txt}</h3>
+        <h4>Cat: {cat}</h4>
         <b>Bold Tag</b>
       </div>
     )
   }
+}
+
+State.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+State.defaultProps = {
+  txt: "This is the default text",
+  cat: 10
 }
 
 export default State
