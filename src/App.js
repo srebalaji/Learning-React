d79d3ecbe07e0ea71d8c8538f6ba3b027de13d23
srebalaji <srebalaji@outlook.com>
2017-07-14 21:31:57 +0530

    React States
    1. Adding State with two new values.
    2. Adding a text field and binding it with the state value.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React from 'react';
 const App = () => <h1>This is stateless</h1>
 
 class State extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "This is default value",
+      cat: 8
+    }
+  }
+
+  update (e) {
+    this.setState({txt: e.target.value})
+  }
+
   render() {
     let txt = this.props.txt
     let cat = this.props.cat
@@ -12,6 +24,9 @@ class State extends React.Component {
         <h3>{txt}</h3>
         <h4>Cat: {cat}</h4>
         <b>Bold Tag</b>
+        <br /><br />
+        Model: <input type="text" onChange={this.update.bind(this)} />
+        <p>{this.state.txt}</p>
       </div>
     )
   }
